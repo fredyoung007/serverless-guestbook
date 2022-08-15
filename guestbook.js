69daa0db583893de347f214c1d7698ade63d51be
fredyoung007 <100824964+fredyoung007@users.noreply.github.com>
2022-08-15 11:29:15 +0800

    Add route API

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5de9a7c2.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
